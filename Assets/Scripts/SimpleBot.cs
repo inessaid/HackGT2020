89008bb21a0d5c71035de345c5340c1f4254ced2
inessaid <43462511+inessaid@users.noreply.github.com>
2020-10-17 12:43:29 -0400

    make a cube appear with ibm

--- Assets/Scripts/SimpleBot.cs
+++ Assets/Scripts/SimpleBot.cs
@@ -224,6 +224,7 @@ public class SimpleBot : MonoBehaviour
         if (targetInputField != null)
         {
             targetInputField.text = textResponse;
+           
         }
 
         // Check if the target GameObject has an InputField then place text into it.
@@ -234,6 +235,106 @@ public class SimpleBot : MonoBehaviour
             {
                 target.text = textResponse;
             }
+        }
+
+        if (response != null && response.Result.Output.Intents.Count != 0)
+        {
+            string intent = response.Result.Output.Intents[0].Intent;
+            Debug.LogError("HERE IS THE INTENT : " + intent);
+
+            bool createdObject = false;
+
+            GameObject myObject = null;
+            Renderer rend = null;
+            string ObjectType = null;
+            Color ObjectColor = Color.red;
+            string currentMat = null;
+
+
+            foreach (RuntimeEntity entity in response.Result.Output.Entities)
+                {
+                    Debug.LogError("entityType: " + entity.Entity + " , value: " + entity.Value);
+                // direction = entity.Value;
+                //gameManager.MoveObject(direction);
+
+                if (entity.Entity == "object")
+                {
+                    //gameManager.CreateObject(entity.Value, currentMat, currentScale);
+                    createdObject = true;
+                    currentMat = null;
+                    //currentScale = null;
+                    ObjectType = entity.Value;
+
+                }
+
+
+                if (entity.Entity == "material")
+                {
+                    currentMat = entity.Value;
+
+                    if (currentMat == "black")
+                    {
+                        ObjectColor = Color.black;
+                    }
+                    else if (currentMat == "blue")
+                    {
+                        ObjectColor = Color.blue;
+                    }
+                    else if (currentMat == "cyan")
+                    {
+                        ObjectColor = Color.cyan;
+                    }
+                    else if (currentMat == "gray")
+                    {
+                        ObjectColor = Color.gray;
+                    }
+                    else if (currentMat == "green")
+                    {
+                        ObjectColor = Color.green;
+                    }
+                    else if (currentMat == "magenta")
+                    {
+                        ObjectColor = Color.magenta;
+                    }
+                    else if (currentMat == "red")
+                    {
+                        ObjectColor = Color.red;
+                    }
+                    else if (currentMat == "white")
+                    {
+                        ObjectColor = Color.white;
+                    }
+                    else if (currentMat == "yellow")
+                    {
+                        ObjectColor = Color.yellow;
+                    }
+
+
+                }
+
+
+                if (ObjectType != null)
+                {
+                    if (ObjectType == "cube")
+                    {
+                        myObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        myObject.transform.position = new Vector3(0f, 1f, 0f);
+                        rend = myObject.GetComponent<Renderer>();
+                        rend.material.color = Color.red;
+                    }
+                    else if (ObjectType == "ball")
+                    {
+                        myObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        myObject.transform.position = new Vector3(0f, 1f, 0f);
+                        rend = myObject.GetComponent<Renderer>();
+                        rend.material.color = Color.red;
+                    }
+                    rend = myObject.GetComponent<Renderer>();
+                    rend.material.color = ObjectColor;
+                }
+
+            }
+
         }
 
         Debug.Log(textResponse);
